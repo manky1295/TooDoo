a10637dfb06b7aa103b5cfecc920433b8a52707e
Akshit Ahluwalia <akshitahluwalia@gmail.com>
2017-06-16 13:16:07 +0530

    DataBase Implemented

--- app.rb
+++ app.rb
@@ -1,72 +1,66 @@
 require 'sinatra'
+require 'data_mapper'
+DataMapper.setup(:default,'sqlite://'+ Dir.pwd + '/project.db')
 
 class ToDoClass
-  attr_accessor :title, :description, :id, :category, :completed, :color
+  include DataMapper::Resource
 
-  def initialize title, description, id, category, urgent: FALSE, important: FALSE, completed: FALSE
-    @title = title
-    @description = description
-    @id = id
-    @category = category
-    @completed = completed
-    if urgent
-      if important
-        @color = "red"
-      else
-        @color = "orange"
-      end
-    else
-      if important
-        @color = "green"
-      else
-        @color = "lightgreen"
-      end
-    end
-  end
+  property :id,  Serial
+  property :title, String
+  property :description, String
+  property :category, String
+  property :completed, Boolean
+  property :urgent, Boolean
+  property :important,  Boolean
+  property :color, String
+
 end
 
-tasks = []
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
-t1 = ToDoClass.new("Shop Groceries", "Shop for Milk , bread etc", 0, 'home', completed: false, urgent: TRUE, important: TRUE)
-t2 = ToDoClass.new("Pay Bills", "Pay Electricity and Broadband bills", 1, 'work', completed: false, urgent: TRUE)
-t3 = ToDoClass.new("Shop Groceries", "Shop for Milk , bread etc", 2, 'home', completed: false, important: TRUE)
-t4 = ToDoClass.new("Pay Bills", "Pay Electricity and Broadband bills", 3, 'work', completed: false)
-
-tasks << t1
-tasks << t2
-tasks << t3
-tasks << t4
+# tasks = []
+#
+# t1 = ToDoClass.new("Shop Groceries", "Shop for Milk , bread etc", 0, 'home', completed: false, urgent: TRUE, important: TRUE)
+# t2 = ToDoClass.new("Pay Bills", "Pay Electricity and Broadband bills", 1, 'work', completed: false, urgent: TRUE)
+# t3 = ToDoClass.new("Shop Groceries", "Shop for Milk , bread etc", 2, 'home', completed: false, important: TRUE)
+# t4 = ToDoClass.new("Pay Bills", "Pay Electricity and Broadband bills", 3, 'work', completed: false)
+#
+# tasks << t1
+# tasks << t2
+# tasks << t3
+# tasks << t4
 
 get '/' do
   data = Hash.new
-  data["tasks"] = tasks
+  data[:tasks] = ToDoClass.all
   erb :index, locals: data
 end
 
 post '/add' do
-  if params["urgent"].to_i == 1
-    urgent = TRUE
+  todo = ToDoClass.new(title: params["task_title"],description: params["task_desc"],category: params["category"], urgent: params["urgent"], important:params["important"],color:"")
+  if todo.urgent
+    if todo.important
+      todo.color = "red"
+    else
+      todo.color = "orange"
+    end
   else
-    urgent = FALSE
+    if important
+      todo.color = "green"
+    else
+      todo.color = "lightgreen"
+    end
   end
-
-  if params["important"].to_i == 1
-    important = TRUE
-  else
-    important = FALSE
-  end
-  todo = ToDoClass.new(params["task_title"], params["task_desc"], tasks.length, params["category"], urgent: urgent, important: important)
-  tasks << todo
+  todo.save
   puts params
   return redirect '/'
 end
 
 post '/state_change' do
-  tasks.each do |task|
-    if task.id == params["taskid"].to_i
-      task.completed = !task.completed
-      return redirect '/'
-    end
-  end
+  task_id = params["taskid"]
+  todo = ToDoClass.get(task_id)
+  todo.completed = !todo.completed
+  todo.save
   return redirect '/'
 end
